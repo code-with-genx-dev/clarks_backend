3136786a0b42fd81c2c42a05b6d864777e1db865
Visva Dev <genxdev25@gmail.com>
2025-07-12 13:23:46 +0530

    config db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './admin/admin.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+     SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'clarks_demo',
+      models: [], // models will be added in their modules
+      autoLoadModels: true,
+      synchronize: true, // WARNING: only use in dev
+    }),AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
