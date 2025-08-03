6768471aefab259a96622878cbbd91f50b8b2a0e
Vishnu-ABSHRMS <vishnu@abshrms.com>
2025-08-03 23:13:55 +0530

    Minor change :: add approvals card details

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete,Headers } from '@nestjs/common';
 import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto/create-product.dto';
+import { approveRejectProductDto, CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { decodeAccessToken } from 'src/common/common';
 
@@ -37,5 +37,26 @@ export class ProductsController {
   async saveProduct(@Headers("Authorization") headers:any,@Body() CreateProductData:CreateProductDto):Promise<any>{
      let token = await decodeAccessToken(headers)
      await this.productsService.saveProduct(+token.user_id,CreateProductData)
+  }
+  @Post("approve-reject-product")
+  async approveRejectProductData(@Headers("Authorization") headers:any,@Body() approveRejectData:approveRejectProductDto):Promise<any>{
+     let token = await decodeAccessToken(headers)
+     approveRejectData.current_user_id =+token.user_id
+     await this.productsService.approveRejectProductData(approveRejectData)
+  }
+  @Get("get-product-details")
+  async getProductDetails(@Headers("Authorization") headers:any):Promise<any>{
+     let token = await decodeAccessToken(headers)
+     await this.productsService.getProductDetails()
+  }
+  @Post("get-product-approval-data")
+  async getProductApprovalData(@Headers("Authorization") headers:any,@Body() data:{user_id: number, selected: Date, status: string, filter: { status: string[] }}):Promise<any>{
+     let token = await decodeAccessToken(headers)
+     await this.productsService.getProductApprovalData(data.user_id, data.selected, data.status, data.filter)
+  }
+  @Post("get-product-approval-card-data")
+  async getProductApprovalcardData(@Headers("Authorization") headers:any,@Body() data:{user_id: number, selected: Date, status: string, filter: { status: string[] }}):Promise<any>{
+     let token = await decodeAccessToken(headers)
+     await this.productsService.getProductApprovalcardData(data.user_id, data.selected, data.status, data.filter)
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
+import { approveRejectProductDto, CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { errorResponse, responseMessageGenerator } from 'src/common/common';
 import { writeFileSync } from 'fs';
@@ -15,11 +15,11 @@ import { Op } from 'sequelize';
 @Injectable()
 export class ProductsService {
 
-    
+
   constructor(
-    @InjectModel(UserRepository) private userModel :typeof UserRepository,
-    @InjectModel(ProductRepository) private ProductModel :typeof ProductRepository,
-  ){}
+    @InjectModel(UserRepository) private userModel: typeof UserRepository,
+    @InjectModel(ProductRepository) private ProductModel: typeof ProductRepository,
+  ) { }
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
@@ -40,136 +40,236 @@ export class ProductsService {
     return `This action removes a #${id} product`;
   }
 
-   private readonly uploadPath = 'public/uploads';
+  private readonly uploadPath = 'public/uploads';
 
 
-   async saveProduct(current_user_id:number,productData:CreateProductDto):Promise<any>{
-    try{
+  async saveProduct(current_user_id: number, productData: CreateProductDto): Promise<any> {
+    try {
 
-       let productDetails:any = await this.ProductModel.findOne({ where: { leather_name:productData.leather_name, leather_category:productData.leather_category} })
-       
-       let leatherImage =await this.uploadUserDetails(productDetails?.id,productData.leather_image, productData.leather_name,"leather")
-       let shoeImage =await this.uploadUserDetails(productDetails?.id,productData.shoe_image, productData.leather_name,"shoe")
+      let productDetails: any = await this.ProductModel.findOne({ where: { leather_name: productData.leather_name, leather_category: productData.leather_category } })
 
-       productData.leather_image  = leatherImage?.data
-       productData.shoe_image  = shoeImage?.data
-       productData.uploaded_by  = current_user_id
-       productData.uploaded_date  = new Date()
-       productData.status  = statusType.Pending
+      let leatherImage = await this.uploadUserDetails(productDetails?.id, productData.leather_image, productData.leather_name, "leather")
+      let shoeImage = await this.uploadUserDetails(productDetails?.id, productData.shoe_image, productData.leather_name, "shoe")
+
+      productData.leather_image = leatherImage?.data
+      productData.shoe_image = shoeImage?.data
+      productData.uploaded_by = current_user_id
+      productData.uploaded_date = new Date()
+      productData.status = statusType.Pending
 
       let updateProductData = await this.ProductModel.upsert({
-              id:productDetails?.id,
-              ...productData})
-       return responseMessageGenerator('success', "data  saved successfully",[])
-    }catch(error){
+        id: productDetails?.id,
+        ...productData
+      })
+      return responseMessageGenerator('success', "data  saved successfully", [])
+    } catch (error) {
       return errorResponse(error.message)
     }
-   }
-   async approveRejectProductData(current_user_id:number,record_id:number,status:string,approved_comments:string):Promise<any>{
-    try{
+  }
+  async approveRejectProductData(approveRejectProduct:approveRejectProductDto): Promise<any> {
+    try {
 
-      let payload ={
-        status:status,
-        approved_comments:approved_comments,
-        approved_user_id:current_user_id,
-        approved_date:new Date()
+         let {current_user_id, record_id, status, approved_comments} = approveRejectProduct
+      let payload = {
+        status: status,
+        approved_comments: approved_comments,
+        approved_user_id: current_user_id,
+        approved_date: new Date()
       }
-       let productDetails:any = await this.ProductModel.findOne({ where: {id:record_id} })
+      let productDetails: any = await this.ProductModel.findOne({ where: { id: record_id } })
 
-       if(productDetails == null){
-             return responseMessageGenerator('failure',"record not found",[])
-       }
-       //update product
-       await this.ProductModel.update(payload,{where:{id:productDetails?.id}})
-       
-       return responseMessageGenerator('success', `product ${status} successfully`,[])
-    }catch(error){
+      if (productDetails == null) {
+        return responseMessageGenerator('failure', "record not found", [])
+      }
+      //update product
+      await this.ProductModel.update(payload, { where: { id: productDetails?.id } })
+
+      return responseMessageGenerator('success', `product ${status} successfully`, [])
+    } catch (error) {
       return errorResponse(error.message)
     }
-   }
+  }
 
-   async getProductApprovalData(user_id:number,selected:Date,status:string,filter:{status:string[]}):Promise<any>{
-    try{
+  async getProductDetails(): Promise<any> {
+    try {
 
-      let whereCondition ={}
-  
-       let startOfMonth  = moment(selected).startOf('month').format('YYYY-MM-DD HH:mm:ss')
-       let endOfMonth = moment(selected).endOf('month').format('YYYY-MM-DD HH:mm:ss')
-       let productDetails:any = await this.ProductModel.findOne({ where: {
-            createdAt:{[Op.between]:[startOfMonth,endOfMonth]},
-            ...(filter.status.length > 0 &&  {status:filter.status} )
-          },
-          include:[
-            {association:"user",attributes:['user_name']}
-          ]
-    })
+      let whereCondition = {}
 
-       let responseData = productDetails.map(singleData =>({
-           uploaded_date: moment(singleData.uploaded_date).format('DD-MMM-YYYY,ddd'),
-           uploaded_by: singleData.users.user_name,
-           upload_category:singleData.leather_category,
-           upload_sub_category:singleData.leather_category,
-           upload_file_name:singleData.leather_category,
-           upload_status:singleData.status
-       }))
-       
-      return responseMessageGenerator('success', "data  saved successfully",responseData)
-    }catch(error){
+      //  let startOfMonth  = moment(selected).startOf('month').format('YYYY-MM-DD HH:mm:ss')
+      //  let endOfMonth = moment(selected).endOf('month').format('YYYY-MM-DD HH:mm:ss')
+      let productDetails: any = await this.ProductModel.findAll({
+        where: { status: "Approved" },
+        include: [
+          { association: "user", attributes: ['user_name'] }
+        ]
+      })
+
+      let responseData = Promise.all(productDetails.map(async singleData => {
+        return {
+          uploaded_date: moment(singleData.uploaded_date).format('DD-MMM-YYYY,ddd'),
+          uploaded_by: singleData.users.user_name,
+          upload_category: singleData.leather_category,
+          upload_sub_category: singleData.leather_category,
+          upload_file_name: singleData.leather_category,
+          upload_status: singleData.status,
+          leather_image: await this.getSignatureAsBase64(singleData.leather_image),
+          show_image: await this.getSignatureAsBase64(singleData.shoe_image)
+        }
+      }
+      ))
+
+      return responseMessageGenerator('success', "data  saved successfully", responseData)
+    } catch (error) {
       return errorResponse(error.message)
     }
-   }
-   async uploadUserDetails(product_id: string, base64Image: string, product_name: string,type:string): Promise<any> {
-        try {
-            // Extract base64 data
-            const matches = base64Image.match(/^data:image\/(\w+);base64,(.+)$/);
-            if (!matches) return responseMessageGenerator('failure', "Invalid image format", null)
+  }
+  async getProductApprovalData(user_id: number, selected: Date, status: string, filter: { status: string[] }): Promise<any> {
+    try {
 
-            let productData :any = await this.ProductModel.findOne({ where: { id: product_id } })
-            let imagePath =productData.shoe_image
-            type =="leather" && (imagePath  =productData.leather_image)
+      let whereCondition = {}
 
-            if (productData && productData.shoe_image) {
-                const oldImagePath = path.join(__dirname, '..', '..', productData.shoe_image);
+      let startOfMonth = moment(selected).startOf('month').format('YYYY-MM-DD HH:mm:ss')
+      let endOfMonth = moment(selected).endOf('month').format('YYYY-MM-DD HH:mm:ss')
+      let productDetails: any = await this.ProductModel.findAll({
+        where: {
+          createdAt: { [Op.between]: [startOfMonth, endOfMonth] },
+          ...(filter.status.length > 0 && { status: filter.status })
+        },
+        include: [
+          { association: "user", attributes: ['user_name'] }
+        ]
+      })
 
-                // Delete old signature file if it exists
-                if (fs.existsSync(oldImagePath)) {
-                    fs.unlinkSync(oldImagePath);
-                }
-            }
+      let responseData = productDetails.map(singleData => ({
+        uploaded_date: moment(singleData.uploaded_date).format('DD-MMM-YYYY,ddd'),
+        uploaded_by: singleData.users.user_name,
+        upload_category: singleData.leather_category,
+        upload_sub_category: singleData.leather_category,
+        upload_file_name: singleData.leather_category,
+        upload_status: singleData.status
+      }))
 
-            let saveSignature = await this.saveSignature(product_id, base64Image,type)
-            if (saveSignature.status == 'failure') {
-                return saveSignature
-            }
-           
-            return responseMessageGenerator('success', "image saved successfully", saveSignature.data)
-        } catch (error) {
-            console.log(error);
-            return errorResponse(error.message)
-        }
+      return responseMessageGenerator('success', "data  saved successfully", responseData)
+    } catch (error) {
+      return errorResponse(error.message)
     }
-    async saveSignature(productId: string, base64Image: string,type:string): Promise<any> {
-        try {
-            // Extract base64 data
-            const matches = base64Image.match(/^data:image\/(\w+);base64,(.+)$/);
-            if (!matches) throw new Error('Invalid image format');
+  }
+  async getProductApprovalcardData(user_id: number, selected: Date, status: string, filter: { status: string[] }): Promise<any> {
+    try {
 
-            const extension = matches[1];
-            const buffer = Buffer.from(matches[2], 'base64');
+      let whereCondition = {}
 
-            let productData:any = await this.userModel.findOne({ where: { id: productId } })
-            // Define file path
-            const fileName = `${(productData.leather_name).toLowerCase()}_${productId}_image.${extension}`;
-            let path = type =="shoe" ? this.uploadPath+"/shoe" : this.uploadPath+"/leather"
-            const filePath = join(path, fileName);
+      let getProductData = async (startOfMonth,endOfMonth,status)=>{
+         let productDetails: any = await this.ProductModel.findAndCountAll({
+        where: {
+          createdAt: { [Op.between]: [startOfMonth, endOfMonth] },
+           ...( status != "Uploads" && {status: filter.status})
+        },
+        include: [
+          { association: "user", attributes: ['user_name'] }
+        ]
+      })
 
-            // Save image
-            writeFileSync(filePath, buffer);
+       return productDetails.count
 
-            return responseMessageGenerator('success', "image saved successfully", filePath)
-        } catch (error) {
-            console.log(error);
-            return responseMessageGenerator('failure', error.message, null)
-        }
+      }
+
+      let startOfMonth = moment(selected).startOf('month').format('YYYY-MM-DD HH:mm:ss')
+      let endOfMonth = moment(selected).endOf('month').format('YYYY-MM-DD HH:mm:ss')
+
+      let statusArray = ["Uploads","Approved","Rejected","Pending"]
+    
+      let responseJson:any =[]
+
+      for(let singleStatus  of statusArray){
+        let Object = {}
+         
+          let statusCount = await getProductData(startOfMonth,endOfMonth,singleStatus)
+           Object['tittle'] = "Total "+singleStatus;
+           Object['count'] = "Total "+statusCount;
+           responseJson.push(Object);
+
+      }
+
+      return responseMessageGenerator('success', "data  saved successfully", responseJson)
+    } catch (error) {
+      return errorResponse(error.message)
     }
+  }
+  async uploadUserDetails(product_id: string, base64Image: string, product_name: string, type: string): Promise<any> {
+    try {
+      // Extract base64 data
+      const matches = base64Image.match(/^data:image\/(\w+);base64,(.+)$/);
+      if (!matches) return responseMessageGenerator('failure', "Invalid image format", null)
+
+      let productData: any = await this.ProductModel.findOne({ where: { id: product_id } })
+      let imagePath = productData.shoe_image
+      type == "leather" && (imagePath = productData.leather_image)
+
+      if (productData && productData.shoe_image) {
+        const oldImagePath = path.join(__dirname, '..', '..', productData.shoe_image);
+
+        // Delete old signature file if it exists
+        if (fs.existsSync(oldImagePath)) {
+          fs.unlinkSync(oldImagePath);
+        }
+      }
+
+      let saveSignature = await this.saveSignature(product_id, base64Image, type)
+      if (saveSignature.status == 'failure') {
+        return saveSignature
+      }
+
+      return responseMessageGenerator('success', "image saved successfully", saveSignature.data)
+    } catch (error) {
+      console.log(error);
+      return errorResponse(error.message)
+    }
+  }
+  async saveSignature(productId: string, base64Image: string, type: string): Promise<any> {
+    try {
+      // Extract base64 data
+      const matches = base64Image.match(/^data:image\/(\w+);base64,(.+)$/);
+      if (!matches) throw new Error('Invalid image format');
+
+      const extension = matches[1];
+      const buffer = Buffer.from(matches[2], 'base64');
+
+      let productData: any = await this.userModel.findOne({ where: { id: productId } })
+      // Define file path
+      const fileName = `${(productData.leather_name).toLowerCase()}_${productId}_image.${extension}`;
+      let path = type == "shoe" ? this.uploadPath + "/shoe" : this.uploadPath + "/leather"
+      const filePath = join(path, fileName);
+
+      // Save image
+      writeFileSync(filePath, buffer);
+
+      return responseMessageGenerator('success', "image saved successfully", filePath)
+    } catch (error) {
+      console.log(error);
+      return responseMessageGenerator('failure', error.message, null)
+    }
+  }
+  async getSignatureAsBase64(filename: string): Promise<any> {
+    try {
+      // Define the file path
+      const filePath = filename
+
+      // Check if file exists
+      if (!fs.existsSync(filePath)) {
+        return responseMessageGenerator('failure', "File not found", null)
+      }
+
+      // Read file and convert to Base64
+      const fileBuffer = fs.readFileSync(filePath);
+      const base64Image = fileBuffer.toString('base64');
+
+      // Return the Base64 response
+      return responseMessageGenerator('success', 'data fetch successfully', `data:image/png;base64,${base64Image}`);
+    } catch (error) {
+      console.error('Error fetching image:', error);
+      responseMessageGenerator('failure', "Error fetching image", null)
+
+    }
+  }
 }
